28d8942a4806c6ccff3e7bb5dd8f1ee3ee13e06f
JevonLiuZz <1468414031@qq.com>
2023-06-06 15:34:47 +0800

    OperateBase added

---
+++ BaseStone/OperateBase.py
@@ -0,0 +1,19 @@
+"""
+import headers
+"""
+
+import torch
+
+######################
+# user code begin
+######################
+
+stripStr = "  this is a string!  \n  "
+stripStrProcessed = stripStr.rstrip()
+print(stripStrProcessed)
+stripStrProcessed = stripStrProcessed.strip()
+print(stripStrProcessed)
+
+######################
+# user code end
+######################
